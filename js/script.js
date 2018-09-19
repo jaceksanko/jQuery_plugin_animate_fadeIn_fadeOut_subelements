44d25578657a793f4552569f443255bed77b6631
Jacek Sańko <jacek.sanko@gmail.com>
2018-09-19 11:08:37 +0200

    Add slideDown, slideUp and animate to plugin

--- js/script.js
+++ js/script.js
@@ -1,43 +1,71 @@
 
 "use strict";
 
-//plugin aniamte subelements, direct children of selected element witch specific parameters: direction (fadeIn/fadeOut), duraction and delay.
+//plugin aniamte subelements, direct children of selected element witch specific parameters: animation (fadeIn/fadeOut/slideDown/slideUp/animate), duraction and delay.
 (function($){
   $.fn.animateSubelements = function(config) {
       const options = $.extend({
-          direction: "fadeIn",
+          animation: "fadeIn",
           duraction: 500,
           delay: 200
       }, config);
 
       return this.each(function() {        
         const subelement = $(this).children()
-        if (options.direction == "fadeIn") {
+        if (options.animation == "fadeIn") {
           subelement.css('display', 'none');
           subelement.each(function(i) {
             $(this).delay(options.delay * (i+1)).fadeIn(options.duraction);
           });
         }
-        if (options.direction == "fadeOut") {
+        if (options.animation == "fadeOut") {
           subelement.each(function(i) {
             $(this).delay(options.delay * (i+1)).fadeOut(options.duraction);
           });
+        }
+        if (options.animation == "slideDown") {
+          subelement.css('display', 'none');
+          subelement.each(function(i) {
+            $(this).delay(options.delay * (i+1)).slideDown(options.duraction);
+          });
         } 
+        if (options.animation == "slideUp") {
+          subelement.each(function(i) {
+            $(this).delay(options.delay * (i+1)).slideUp(options.duraction);
+          });
+        }
+        if (options.animation == "animate") {
+          subelement.each(function(i) {
+            $(this).delay(options.delay * (i+1)).animate(options.properity, options.duraction);
+          });
+        }
       });
   }
 })(jQuery);
 
 
 //Examples runing
+$(".test1").animateSubelements({
+  animation: "slideDown",
+  duraction: 500,
+  delay: 500
+}) 
+
 
 $("div").animateSubelements({
-  direction: "fadeIn",
+  animation: "fadeIn",
   duraction: 500,
   delay: 500
-})
+}) 
 
 $(".test2").animateSubelements({
-  direction: "fadeOut",
+  animation: "animate",
   duraction: 500,
-  delay: 500
+  delay: 500,
+  properity: {
+    width: "70%",
+    opacity: 0.4,
+    marginLeft: "0.6in",
+    fontSize: "3em",
+    borderWidth: "10px"}
 })
