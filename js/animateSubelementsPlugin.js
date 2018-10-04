21198bfa2d5266ddbadcff78b184043c7983c363
Jacek Sańko <jacek.sanko@gmail.com>
2018-10-04 15:29:20 +0200

    cleared package.json, changed plug-in function to a more general one and put to another component

---
+++ js/animateSubelementsPlugin.js
@@ -0,0 +1,27 @@
+/* 
+plugin animate subelements, direct children of selected element witch specific parameters: 
+animation type,  properitys specific for aniamtion type (in array), delay and 
+displaySub (none if animation subelement must display none before animation start or "" if not). 
+*/
+export default (function($) {
+    $.fn.animateSubelements = function(config) {
+        const options = $.extend(
+            {
+                animation: "fadeOut",
+                properity: [500],
+                delay: 500,
+                displaySub: ""
+            },
+            config
+        );
+        return this.each(function() {
+            const subelement = $(this).children();
+            subelement.css("display", options.displaySub);
+            subelement.each(function(i) {
+                $(this).delay(options.delay * (i + 1));
+                $(this)[options.animation](...options.properity);
+            });
+        });
+    };
+})(jQuery);
+
--- js/script.js
+++ js/script.js
@@ -1,71 +1,37 @@
-
-"use strict";
-
-//plugin aniamte subelements, direct children of selected element witch specific parameters: animation (fadeIn/fadeOut/slideDown/slideUp/animate), duraction and delay.
-(function($){
-  $.fn.animateSubelements = function(config) {
-      const options = $.extend({
-          animation: "fadeIn",
-          duraction: 500,
-          delay: 200
-      }, config);
-
-      return this.each(function() {        
-        const subelement = $(this).children()
-        if (options.animation == "fadeIn") {
-          subelement.css('display', 'none');
-          subelement.each(function(i) {
-            $(this).delay(options.delay * (i+1)).fadeIn(options.duraction);
-          });
-        }
-        if (options.animation == "fadeOut") {
-          subelement.each(function(i) {
-            $(this).delay(options.delay * (i+1)).fadeOut(options.duraction);
-          });
-        }
-        if (options.animation == "slideDown") {
-          subelement.css('display', 'none');
-          subelement.each(function(i) {
-            $(this).delay(options.delay * (i+1)).slideDown(options.duraction);
-          });
-        } 
-        if (options.animation == "slideUp") {
-          subelement.each(function(i) {
-            $(this).delay(options.delay * (i+1)).slideUp(options.duraction);
-          });
-        }
-        if (options.animation == "animate") {
-          subelement.each(function(i) {
-            $(this).delay(options.delay * (i+1)).animate(options.properity, options.duraction);
-          });
-        }
-      });
-  }
-})(jQuery);
-
+import animateSubelementsPlugin  from "./animateSubelementsPlugin.js";
 
 //Examples runing
 $(".test1").animateSubelements({
-  animation: "slideDown",
-  duraction: 500,
-  delay: 500
-}) 
+    animation: "slideDown",
+    properity: [500],
+    delay: 500,
+    displaySub: "none"
+});
 
+$(".test3").animateSubelements({
+    animation: "slideUp",
+    properity: [500],
+    delay: 500
+});
 
 $("div").animateSubelements({
-  animation: "fadeIn",
-  duraction: 500,
-  delay: 500
-}) 
+    animation: "fadeIn",
+    properity: [600],
+    delay: 600,
+    displaySub: "none"
+});
 
 $(".test2").animateSubelements({
-  animation: "animate",
-  duraction: 500,
-  delay: 500,
-  properity: {
-    width: "70%",
-    opacity: 0.4,
-    marginLeft: "0.6in",
-    fontSize: "3em",
-    borderWidth: "10px"}
-})
+    animation: "animate",
+    properity: [
+        {
+            width: "70%",
+            opacity: 0.4,
+            marginLeft: "0.6in",
+            fontSize: "3em",
+            borderWidth: "10px"
+        },
+        500
+    ],
+    delay: 500
+});
